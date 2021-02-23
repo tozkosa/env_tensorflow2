9acadddcb0c04b23e19b5948d61db2dd119541ec
Tomoko Ozeki <tomoko.ozeki@gmail.com>
2021-02-24 07:34:30 +0900

    Add dropout

--- python/make_tensorboard.py
+++ python/make_tensorboard.py
@@ -9,6 +9,7 @@ def make_tensorboard(set_dir_name=''):
   tictoc = strftime("%a_%d_%b_%Y_%H_%M_%S", gmtime())
   directory_name = tictoc
   log_dir = set_dir_name + '_' + directory_name
+  log_dir = os.path.join("rec", log_dir)
   os.mkdir(log_dir)
   tensorboard = TensorBoard(log_dir=log_dir)
   return tensorboard
